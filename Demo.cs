71f15007a7745ed882887bf9371025b46210d953
zouhangte <zouhangtezbm@126.com>
2017-03-29 19:09:05 +0800

    add demo

---
+++ Demo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class Demo : MonoBehaviour
+{
+    public InputField textbox;
+    public Button tranBtn;
+    public Button clearBtn;
+    public AudioSource audio;
+
+    Txt2Audio.Text2AudioCtrl ctrl;
+    void Start()
+    {
+        tranBtn.onClick.AddListener(RequireAudio);
+        clearBtn.onClick.AddListener(RemoveLocal);
+        ctrl = Txt2Audio.Text2AudioCtrl.Instance;
+    }
+    void RequireAudio()
+    {
+        StartCoroutine(ctrl.GetAudioClip(textbox.text, OnGet));
+    }
+
+    void RemoveLocal()
+    {
+        ctrl.CleanUp();
+    }
+
+    void OnGet(AudioClip clip)
+    {
+        audio.PlayOneShot(clip);
+    }
+}
